4531c22ae6acc3ada1a88762904347d4d1090583
Unmesh Bari <unmeshbari5@gmail.com>
2025-11-02 23:01:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
+	let finalCost = 0
+	while (arr.length>1) {
+		let sortedArr = sortArray(arr);
+
+		let first = sortArray[0]
+		let second = sortArray[1]
+
+		let cost  = first + second;
+		finalCost +=cost;
+
+		sortedArr.splice(0,2)
+		sortedArr.push(cost)
+		arr = sortedArr
+		
+		
+	}
   
+}
+
+function sortArray(arr) {
+	for(let i=1; i<arr.length; i++){
+		let j= i;
+		while (j>0 &&  arr[j]< arr[j-1]) {
+			let temp = arr[j]
+			arr[j] = arr[j-1]
+			arr[j-1] = temp
+			j--
+		}
+	}
+	return arr;	
 }
 
 module.exports=mincost;
